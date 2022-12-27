62a8cf78d9121bf336d40aa880b4868f409ec632
Stanley Wintergreen <kepocnhh@gmail.com>
2022-12-27 22:19:08 +0300

    Return Values and Scope

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,15 @@ fn main() {
     let x = 5;                      // x comes into scope
     makes_copy(x);                  // x would move into the function
     // let s1 = s; // ERR: value used here after move
+
+    // Return Values and Scope
+    let s1 = gives_ownership();
+    let s2 = String::from("hello");
+    let s3 = takes_and_gives_back(s2);
+
+    let s1 = String::from("hello");
+    let (s2, len) = calculate_length(s1);
+    println!("The length of '{}' is {}.", s2, len);
 }
 
 fn takes_ownership(some_string: String) {
@@ -23,4 +32,18 @@ fn takes_ownership(some_string: String) {
 
 fn makes_copy(some_integer: i32) {
     println!("{}", some_integer);
+}
+
+fn gives_ownership() -> String {
+    let some_string = String::from("yours");
+    return some_string
+}
+
+fn takes_and_gives_back(a_string: String) -> String {
+    return a_string
+}
+
+fn calculate_length(s: String) -> (String, usize) {
+    let length = s.len();
+    (s, length)
 }
