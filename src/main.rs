2cf5b8b9a9cfef1a235e6a979c49fdeb7ecc4c9b
Stanley Wintergreen <kepocnhh@gmail.com>
2022-12-27 22:14:37 +0300

    Ownership and Functions

--- src/main.rs
+++ src/main.rs
@@ -8,4 +8,19 @@ fn main() {
     let let_string = String::from("baz");
     let s0 = let_string; // Ownership is moved here
     // let s1 = let_string; // ERR: value used here after move
+
+    // functions
+    let s = String::from("hello");  // s comes into scope
+    takes_ownership(s);             // s's value moves into the function...
+    let x = 5;                      // x comes into scope
+    makes_copy(x);                  // x would move into the function
+    // let s1 = s; // ERR: value used here after move
+}
+
+fn takes_ownership(some_string: String) {
+    println!("{}", some_string);
+}
+
+fn makes_copy(some_integer: i32) {
+    println!("{}", some_integer);
 }
