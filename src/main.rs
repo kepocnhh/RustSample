3b2a2ceed46773481fa7da669499241f228826ce
Stanley Wintergreen <kepocnhh@gmail.com>
2022-12-29 16:30:09 +0300

    Mutable References

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,16 @@ fn main() {
     let s = String::from("foo");
     let len = calculate_length(&s);
     println!("The length of '{}' is {}.", s, len);
+
+    let mut ms = String::from("bar");
+    change(&mut ms);
+    println!("Mutable: '{}'", ms);
 }
 
 fn calculate_length(s: &String) -> usize {
     s.len()
+}
+
+fn change(s: &mut String) {
+    s.push_str(", world");
 }
