327628acf7a57fb98ce2668a2295fe1dc3ee8784
Stanley Wintergreen <kepocnhh@gmail.com>
2023-01-03 21:48:30 +0300

    Unit-Like Structs

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,10 @@ fn main() {
     let origin = Point(0, 0, 0);
     println!("color: {}", black.1);
     println!("point: {}", origin.1);
+    let subject = AlwaysEqual;
+    // assert_eq!(subject, subject)
 }
 
 struct Color(i32, i32, i32);
 struct Point(i32, i32, i32);
+struct AlwaysEqual;
