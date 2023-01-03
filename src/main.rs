3ebc2c03d1c6c2bb630c22bab2e91b7f149c1cf3
Stanley Wintergreen <kepocnhh@gmail.com>
2023-01-03 21:33:59 +0300

    Defining and Instantiating Structs | function body

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,8 @@
 fn main() {
-    let mut user = User {
-        active: true,
-        username: String::from("someusername123"),
-        email: String::from("someone@example.com"),
-        sign_in_count: 1,
-    };
-    user.username = String::from("anotheremail@example.com");
+    let user = build_user(
+        String::from("someone@example.com"),
+        String::from("someusername123")
+    );
     println!("User: {}", user.username)
 }
 
@@ -14,4 +11,13 @@ struct User {
     username: String,
     email: String,
     sign_in_count: u64,
+}
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        active: true,
+        username: username,
+        email: email,
+        sign_in_count: 1,
+    }
 }
