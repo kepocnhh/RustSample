72479ac0ac59126dc1e7c0e84f58dc64a63935fb
Stanley Wintergreen <kepocnhh@gmail.com>
2023-01-03 21:46:11 +0300

    Tuple Structs

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,9 @@
 fn main() {
-    let src = build_user(
-        String::from("someone@example.com"),
-        String::from("someusername123")
-    );
-    let u0 = User {
-        active: src.active,
-        username: String::from("u0username"),
-        email: String::from("u0@example.com"),
-        sign_in_count: src.sign_in_count,
-    };
-    println!("User: {}", u0.username);
-    let u1 = User {
-        username: String::from("u1username"),
-        email: String::from("u1@example.com"),
-        ..u0
-    };
-    println!("User: {}", u1.username);
+    let black = Color(0, 0, 0);
+    let origin = Point(0, 0, 0);
+    println!("color: {}", black.1);
+    println!("point: {}", origin.1);
 }
 
-struct User {
-    active: bool,
-    username: String,
-    email: String,
-    sign_in_count: u64,
-}
-
-fn build_user(email: String, username: String) -> User {
-    User {
-        active: true,
-        username,
-        email,
-        sign_in_count: 1,
-    }
-}
+struct Color(i32, i32, i32);
+struct Point(i32, i32, i32);
