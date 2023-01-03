cba8755cba81f66a54c3df7dd77c76c9afe59fca
Stanley Wintergreen <kepocnhh@gmail.com>
2023-01-03 15:52:04 +0300

    String Slices

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,20 @@
 fn main() {
-    let text = String::from("foo bar baz");
-    let i = first_word(&text);
-    println!("first word: {}", i);
-}
-
-fn first_word(s: &String) -> usize {
-    for (i, &item) in s.as_bytes().iter().enumerate() {
-        if item == b' ' {
-            return i;
-        }
-    }
-    s.len()
+    let s = String::from("hello world");
+    let slice = &s[0..5];
+    println!("slice [0..5]: \"{}\"", slice);
+    let slice = &s[6..11];
+    println!("slice [6..11]: \"{}\"", slice);
+    let slice = &s[..5];
+    println!("slice [..5]: \"{}\"", slice);
+    let len = s.len();
+    let slice = &s[6..len];
+    println!("slice [6..len]: \"{}\"", slice);
+    let slice = &s[6..s.len()];
+    println!("slice [6..s.len()]: \"{}\"", slice);
+    let slice = &s[6..];
+    println!("slice [6..]: \"{}\"", slice);
+    let slice = &s[0..len];
+    println!("slice [0..len]: \"{}\"", slice);
+    let slice = &s[..];
+    println!("slice [..]: \"{}\"", slice);
 }
