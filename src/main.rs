e49420ea0f7339e6f9914a648d2d7127952accd6
Stanley Wintergreen <kepocnhh@gmail.com>
2023-01-04 17:07:34 +0300

    Matching with Option<T>

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,12 @@
-use crate::UsState::{Alabama, Alaska};
-
 fn main() {
-    for it in [Coin::Penny, Coin::Nickel, Coin::Dime, Coin::Quarter(Alabama), Coin::Quarter(Alaska)] {
-        let value = value_in_cents(&it);
-        println!("coin: {:?} -> value: {}", it, value);
+    for it in [Some(5), Some(42), None] {
+        println!("{:?} + 1 = {:?}", it, plus_one(it));
     }
 }
 
-#[derive(Debug)]
-enum UsState {
-    Alabama,
-    Alaska,
-}
-
-#[derive(Debug)]
-enum Coin {
-    Penny,
-    Nickel,
-    Dime,
-    Quarter(UsState),
-}
-
-fn value_in_cents(coin: &Coin) -> u8 {
-    match coin {
-        Coin::Penny => 1,
-        Coin::Nickel => 5,
-        Coin::Dime => 10,
-        Coin::Quarter(_state) => 25,
+fn plus_one(it: Option<i32>) -> Option<i32> {
+    match it {
+        None => None,
+        Some(i) => Some(i + 1),
     }
 }
