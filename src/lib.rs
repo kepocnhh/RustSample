6d7d2211607df41dde1edff1ec24465f76ac86da
Stanley Wintergreen <kepocnhh@gmail.com>
2023-01-04 21:09:00 +0300

    Using External Packages

--- src/lib.rs
+++ src/lib.rs
@@ -1,45 +1,6 @@
-mod foo {
-    #[derive(Debug)]
-    pub struct Bar {
-        pub i_public: u8,
-        i_private: String
-    }
-
-    impl Bar {
-        pub fn new() -> Bar {
-            Bar {
-                i_public: 1,
-                i_private: String::from("foo")
-            }
-        }
-    }
-
-    pub mod bar {
-        pub fn println() {
-            println!("foo bar");
-        }
-    }
-}
-
-use crate::foo::bar::println;
-
-mod baz {
-    pub fn function() {
-        super::println()
-    }
-}
-
-use std::fmt;
-use std::io::Result as IoResult;
-
-fn function1() -> fmt::Result {
-    todo!()
-}
-
-fn function2() -> IoResult<()> {
-    todo!()
-}
+use rand::Rng;
 
 pub fn run() {
-    baz::function()
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    println!("secret number: {secret_number}")
 }
