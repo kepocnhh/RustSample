4659ce3468889f604890203362c570b9a9d12652
Stanley Wintergreen <kepocnhh@gmail.com>
2023-01-04 17:26:10 +0300

    Concise Control Flow with if let

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,20 @@
 fn main() {
-    let dice_roll = 9;
-    match dice_roll {
-        3 => println!("dice: 3"),
-        7 => println!("dice: 7"),
-        other => println!("dice: {other}"),
-        _ => todo!("Impossible!"),
-        _ => ()
+    for it in [Some(1), Some(42), None] {
+        match it {
+            Some(value) => println!("match Some: {}", value),
+            _ => (),
+        }
+        if let Some(value) = it {
+            println!("if let Some: {}", value)
+        }
+        match it {
+            Some(value) => println!("match Some: {}", value),
+            _ => println!("match else: {:?}", it)
+        }
+        if let Some(value) = it {
+            println!("if let Some: {}", value)
+        } else {
+            println!("if let else: {:?}", it)
+        }
     }
 }
