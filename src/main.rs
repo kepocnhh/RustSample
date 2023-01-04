90d253bb3abf2e9f972c20edd49e297b9f053648
Stanley Wintergreen <kepocnhh@gmail.com>
2023-01-04 16:19:02 +0300

    attach data (u8, u8, u8, u8)...

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
 fn main() {
-    let home = IpAddr::V4(String::from("127.0.0.1"));
+    let home = IpAddr::V4(127, 0, 0, 1);
     route(home);
     let loopback = IpAddr::V6(String::from("::1"));
     route(loopback);
-    route(IpAddr::V4(String::from("foo")));
+    route(IpAddr::V4(128, 0, 0, 1));
     route(IpAddr::V6(String::from("bar")));
 }
 
 #[derive(Debug)]
 enum IpAddr {
-    V4(String),
+    V4(u8, u8, u8, u8),
     V6(String),
 }
 
