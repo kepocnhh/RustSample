ec653568b8e592b2c3e5e02549c594b9e2878926
Stanley Wintergreen <kepocnhh@gmail.com>
2023-01-04 16:18:10 +0300

    attach data...

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,18 @@
 fn main() {
-    let four = IpAddrKind::V4;
-    route(four);
-    let six = IpAddrKind::V6;
-    route(six);
-    route(IpAddrKind::V4);
-    route(IpAddrKind::V6);
+    let home = IpAddr::V4(String::from("127.0.0.1"));
+    route(home);
+    let loopback = IpAddr::V6(String::from("::1"));
+    route(loopback);
+    route(IpAddr::V4(String::from("foo")));
+    route(IpAddr::V6(String::from("bar")));
 }
 
 #[derive(Debug)]
-enum IpAddrKind {
-    V4,
-    V6,
+enum IpAddr {
+    V4(String),
+    V6(String),
 }
 
-fn route(ip_kind: IpAddrKind) {
-    println!("{:?}", ip_kind)
+fn route(item: IpAddr) {
+    println!("{:?}", item)
 }
