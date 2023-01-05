5fefd15265f92810aab078b89370eee1cb82be9e
Stanley Wintergreen <kepocnhh@gmail.com>
2023-01-05 19:53:23 +0300

    Creating a New String

--- src/book/ch0802.rs
+++ src/book/ch0802.rs
@@ -1,4 +1,12 @@
 pub fn run() {
     println!("\nStoring UTF-8 Encoded Text with Strings");
-    todo!()
+    new_string()
+}
+
+fn new_string() {
+    let mut s = String::from("foo");
+    s.push_str("bar");
+    println!("string: {s}");
+    s.push('l');
+    println!("string: {s}");
 }
