34965e0407f114bd1a2f48aad3243334754f1035
Stanley Wintergreen <kepocnhh@gmail.com>
2023-01-06 16:19:41 +0300

    Storing Keys with Associated Values in Hash Maps

---
+++ src/book/ch0803.rs
@@ -0,0 +1,8 @@
+pub fn run() {
+    const CHAPTER: u8 = 8;
+    const PART: u8 = 3;
+    const TITLE: &str = "Storing Keys with Associated Values in Hash Maps";
+    println!("\n\t{:02}/{:02}\t\"{TITLE}\"", CHAPTER, PART);
+
+    todo!()
+}
