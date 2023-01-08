9cc1d89db3f58cd9edb221179d23562c9e239793
Stanley Wintergreen <kepocnhh@gmail.com>
2023-01-09 00:03:56 +0300

    Matching on Different Errors

--- src/book/ch0902.rs
+++ src/book/ch0902.rs
@@ -1,4 +1,5 @@
 use std::fs::File;
+use std::io::ErrorKind;
 
 pub fn run() {
     const CHAPTER: u8 = 9;
@@ -7,6 +8,7 @@ pub fn run() {
     println!("\n\t{:02}/{:02}\t\"{TITLE}\"", CHAPTER, PART);
 
     open_file();
+    matching_errors();
 }
 
 fn open_file() {
@@ -14,5 +16,21 @@ fn open_file() {
     match File::open(name) {
         Ok(file) => println!("File: {:?}", file),
         Err(error) => println!("Error: file \"{name}\" {:?}", error),
+    };
+}
+
+fn matching_errors() {
+    println!("\nMatching on Different Errors");
+
+    let name = "foo.txt";
+    match File::open(name) {
+        Ok(file) => println!("open file: {:?}", file),
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create(name) {
+                Ok(file) => println!("create file: {:?}", file),
+                Err(error) => println!("create file \"{name}\" error: {:?}", error)
+            },
+            _ => println!("open file \"{name}\" error: {:?}", error)
+        }
     };
 }
