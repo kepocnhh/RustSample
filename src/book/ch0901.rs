8a435910a7e8e0c08dc84f7e1ac9f4bb27ad17c6
Stanley Wintergreen <kepocnhh@gmail.com>
2023-01-08 22:49:34 +0300

    chapter 09

---
+++ src/book/ch0901.rs
@@ -0,0 +1,8 @@
+pub fn run() {
+    const CHAPTER: u8 = 9;
+    const PART: u8 = 1;
+    const TITLE: &str = "Unrecoverable Errors with panic!";
+    println!("\n\t{:02}/{:02}\t\"{TITLE}\"", CHAPTER, PART);
+
+    todo!()
+}
--- src/book/mod.rs
+++ src/book/mod.rs
@@ -1,9 +1,11 @@
 pub mod ch0801;
 pub mod ch0802;
 pub mod ch0803;
+pub mod ch0901;
 
 pub fn run() {
     ch0801::run();
     ch0802::run();
     ch0803::run();
+    ch0901::run();
 }
