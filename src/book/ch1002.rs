b3bf0e101f4e155a10e280e4502a702dba1f7669
Stanley Wintergreen <kepocnhh@gmail.com>
2023-01-11 13:58:11 +0300

    Defining a Trait

--- src/book/ch1002.rs
+++ src/book/ch1002.rs
@@ -4,5 +4,26 @@ pub fn run() {
     const TITLE: &str = "Traits: Defining Shared Behavior";
     println!("\n\t{:02}/{:02}\t\"{TITLE}\"", CHAPTER, PART);
 
-    todo!();
+    defining_a_trait();
+}
+
+fn defining_a_trait() {
+    println!("\nDefining a Trait");
+
+    let foo = Foo { value: "bar".to_string() };
+    println!("summary: {}", foo.summarize())
+}
+
+trait Summary {
+    fn summarize(&self) -> String;
+}
+
+struct Foo {
+    value: String,
+}
+
+impl Summary for Foo {
+    fn summarize(&self) -> String {
+        format!("value: {}", self.value)
+    }
 }
