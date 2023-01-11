7114f31205e755820681b75c98cfc8f1bd2e97ab
Stanley Wintergreen <kepocnhh@gmail.com>
2023-01-11 14:59:05 +0300

    Validating References with Lifetimes

---
+++ src/book/ch1003.rs
@@ -0,0 +1,8 @@
+pub fn run() {
+    const CHAPTER: u8 = 10;
+    const PART: u8 = 3;
+    const TITLE: &str = "Validating References with Lifetimes";
+    println!("\n\t{:02}/{:02}\t\"{TITLE}\"", CHAPTER, PART);
+
+    todo!()
+}
--- src/book/mod.rs
+++ src/book/mod.rs
@@ -5,6 +5,7 @@ pub mod ch0901;
 pub mod ch0902;
 pub mod ch1001;
 pub mod ch1002;
+pub mod ch1003;
 
 pub fn run() {
     ch0801::run();
@@ -14,4 +15,5 @@ pub fn run() {
     ch0902::run();
     ch1001::run();
     ch1002::run();
+    ch1003::run();
 }
