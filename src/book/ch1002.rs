1ac8748038bd100e07b9db6332f89e93a66c40f4
Stanley Wintergreen <kepocnhh@gmail.com>
2023-01-11 13:47:30 +0300

    Traits: Defining Shared Behavior

---
+++ src/book/ch1002.rs
@@ -0,0 +1,8 @@
+pub fn run() {
+    const CHAPTER: u8 = 10;
+    const PART: u8 = 2;
+    const TITLE: &str = "Traits: Defining Shared Behavior";
+    println!("\n\t{:02}/{:02}\t\"{TITLE}\"", CHAPTER, PART);
+
+    todo!();
+}
