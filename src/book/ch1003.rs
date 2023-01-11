2f48dd029f0084642198766ee480d2cb54b40570
Stanley Wintergreen <kepocnhh@gmail.com>
2023-01-11 18:46:35 +0300

    Generic Lifetimes in Functions

--- src/book/ch1003.rs
+++ src/book/ch1003.rs
@@ -4,7 +4,8 @@ pub fn run() {
     const TITLE: &str = "Validating References with Lifetimes";
     println!("\n\t{:02}/{:02}\t\"{TITLE}\"", CHAPTER, PART);
 
-    preventing_dangling()
+    preventing_dangling();
+    generic_lifetimes();
 }
 
 fn preventing_dangling() {
@@ -31,3 +32,48 @@ fn preventing_dangling_1() {
                           //   |       |
     println!("r: {}", r); //   |       |
 }                         // --+-------+
+
+fn generic_lifetimes() {
+    println!("\nGeneric Lifetimes in Functions");
+
+    let string1 = String::from("abcd");
+    let string2 = "xyz";
+
+    let result = longest(string1.as_str(), string2);
+    println!("The longest string is \"{}\"", result);
+
+    let r;
+    let a = "abcd";
+    {
+        let b = "xyz";
+        r = longest(a, b);
+        // let b = String::from("xyz");
+        // r = longest(a, b.as_str()); // error ?
+    }
+    println!("r \"{r}\"");
+
+    let r;
+    let a = 1;
+    {
+        // let b = 2;
+        // r = max(&a, &b); // error
+        r = max(&a, &3);
+    }
+    println!("r \"{r}\"");
+}
+
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
+fn max<'a>(x: &'a i32, y: &'a i32) -> &'a i32 {
+    if x > y {
+        x
+    } else {
+        y
+    }
+}
