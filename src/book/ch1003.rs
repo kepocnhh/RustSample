ccf3cac87f6a0fb4ba2e86f1a1da3d121cdd34b1
Stanley Wintergreen <kepocnhh@gmail.com>
2023-01-11 19:04:06 +0300

    Lifetime Annotations in Struct Definitions

--- src/book/ch1003.rs
+++ src/book/ch1003.rs
@@ -6,6 +6,7 @@ pub fn run() {
 
     preventing_dangling();
     generic_lifetimes();
+    in_struct_definitions();
 }
 
 fn preventing_dangling() {
@@ -76,4 +77,21 @@ fn max<'a>(x: &'a i32, y: &'a i32) -> &'a i32 {
     } else {
         y
     }
+}
+
+fn in_struct_definitions() {
+    println!("\nLifetime Annotations in Struct Definitions");
+
+    let novel = String::from("Call me Ishmael. Some years ago...");
+    let first_sentence = novel.split('.').next().expect("Could not find a '.'");
+    let expert = Expert { part: first_sentence };
+    in_struct_definitions_0(&expert);
+}
+
+struct Expert<'a> {
+    part: &'a str,
+}
+
+fn in_struct_definitions_0(expert: &Expert) {
+    println!("expert of \"{}\"", expert.part);
 }
