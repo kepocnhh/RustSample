d2ea0b2fc178dd20feb00013cb2d5ab543ebbdfb
Stanley Wintergreen <kepocnhh@gmail.com>
2023-01-11 15:09:23 +0300

    Preventing Dangling References with Lifetimes

--- src/book/ch1003.rs
+++ src/book/ch1003.rs
@@ -4,5 +4,30 @@ pub fn run() {
     const TITLE: &str = "Validating References with Lifetimes";
     println!("\n\t{:02}/{:02}\t\"{TITLE}\"", CHAPTER, PART);
 
-    todo!()
+    preventing_dangling()
 }
+
+fn preventing_dangling() {
+    println!("\nPreventing Dangling References with Lifetimes");
+
+    preventing_dangling_0();
+    preventing_dangling_1();
+}
+
+fn preventing_dangling_0() {
+    let r;                // ---------+-- 'r
+    {                     //          |
+        let x = 5;        // -+-- 'x  |
+        // r = &x;        //  |       | // borrow checker error
+        r = x;            //  |       |
+    }                     // -+       |
+    println!("r: {}", r); //          |
+}                         // ---------+
+
+fn preventing_dangling_1() {
+    let x = 5;            // ----------+-- 'x
+                          //           |
+    let r = &x;           // --+-- 'r  |
+                          //   |       |
+    println!("r: {}", r); //   |       |
+}                         // --+-------+
