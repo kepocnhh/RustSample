f37fefe7eb47526d5de692ce2b692fa0e3710033
Stanley Wintergreen <kepocnhh@gmail.com>
2023-01-12 14:31:47 +0300

    Writing Automated Tests

---
+++ src/book/ch1101.rs
@@ -0,0 +1,8 @@
+pub fn run() {
+    const CHAPTER: u8 = 11;
+    const PART: u8 = 1;
+    const TITLE: &str = "How to Write Tests";
+    println!("\n\t{:02}/{:02}\t\"{TITLE}\"", CHAPTER, PART);
+
+    todo!()
+}
