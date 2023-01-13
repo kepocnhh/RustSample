f2eeacefa11aff09f606d4d148242b98569aa448
Stanley Wintergreen <kepocnhh@gmail.com>
2023-01-13 16:56:38 +0300

    cargo test -- --include-ignored

--- src/book/ch1102.rs
+++ src/book/ch1102.rs
@@ -13,7 +13,8 @@ fn test_help() {
         "cargo test -- --show-output",
         "cargo test {test_name}",
         "cargo test {test_name_prefix}",
-        "cargo test -- --ignored",
+        "cargo test -- --ignored", // only ignored
+        "cargo test -- --include-ignored", // with ignored
     ];
     for (index, it) in commands.iter().enumerate() {
         println!("{index}] {it}")
