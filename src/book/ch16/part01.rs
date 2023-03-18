2d8be61240c0edef807b3181b4c0c6425b680b30
Stanley Wintergreen <kepocnhh@gmail.com>
2023-03-18 14:37:04 +0300

    Waiting for All Threads to Finish Using join Handles

--- src/book/ch16/part01.rs
+++ src/book/ch16/part01.rs
@@ -7,6 +7,7 @@ pub fn run() {
     println!("\n\t{:02}/{:02}\t\"{TITLE}\"", CHAPTER, PART);
 
     _0101();
+    _0102();
     todo!();
 }
 
@@ -14,10 +15,11 @@ fn _0101() {
     println!("\nCreating a New Thread with spawn");
 
     std::thread::spawn(|| {
+        let subpart = 1;
         let current = std::thread::current();
         let name = current.name().unwrap_or("spawned");
         for i in 1..10 {
-            println!("hi number {i} from the \"{name}\" thread!");
+            println!("{subpart:02}] number {i} from the \"{name}\" thread!");
             std::thread::sleep(Duration::from_millis(1));
         }
     });
@@ -26,4 +28,25 @@ fn _0101() {
         println!("hi number {i} from the {:?} thread!", std::thread::current().name());
         std::thread::sleep(Duration::from_millis(1));
     }
+}
+
+fn _0102() {
+    println!("\nWaiting for All Threads to Finish Using join Handles");
+
+    let handle = std::thread::spawn(|| {
+        let subpart = 2;
+        let current = std::thread::current();
+        let name = current.name().unwrap_or("spawned");
+        for i in 1..10 {
+            println!("{subpart:02}] number {i} from the \"{name}\" thread!");
+            std::thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    for i in 1..3 {
+        println!("hi number {i} from the {:?} thread!", std::thread::current().name());
+        std::thread::sleep(Duration::from_millis(1));
+    }
+
+    handle.join().unwrap();
 }
