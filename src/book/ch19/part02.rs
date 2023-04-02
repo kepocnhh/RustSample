351d4f83262832616f582dd1d4bbb589c7ae6daf
Stanley Wintergreen <kepocnhh@gmail.com>
2023-04-02 18:41:20 +0300

    Default Generic Type Parameters and Operator Overloading

--- src/book/ch19/part02.rs
+++ src/book/ch19/part02.rs
@@ -1,3 +1,5 @@
+use std::ops::Add;
+
 pub fn run() {
     const CHAPTER: u8 = 19;
     const PART: u8 = 2;
@@ -5,6 +7,7 @@ pub fn run() {
     println!("\n\t{:02}/{:02}\t\"{TITLE}\"", CHAPTER, PART);
 
     _01();
+    _02();
     todo!();
 }
 
@@ -41,4 +44,32 @@ fn _01() {
     let bar = vec![String::from("baz")];
     let any = bar.get_any();
     println!("bar any: {any}");
+}
+
+struct MyU8(u8);
+
+impl Add for MyU8 {
+    type Output = MyU8;
+    fn add(self, rhs: Self) -> Self::Output {
+        return MyU8(self.0 + rhs.0)
+    }
+}
+
+impl Add<u8> for MyU8 {
+    type Output = MyU8;
+
+    fn add(self, rhs: u8) -> Self::Output {
+        return MyU8(self.0 + rhs)
+    }
+}
+
+fn _02() {
+    println!("\nDefault Generic Type Parameters and Operator Overloading");
+
+    let m1 = MyU8(2);
+    let m2 = MyU8(3);
+    let m3: u8 = 4;
+    assert_eq!(m1.0 + m2.0, 5);
+    assert_eq!(m2.0 + m3, 7);
+    println!("m1: {}, m2: {}, m3:{m3}", m1.0, m2.0);
 }
