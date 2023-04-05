9344a57b38436e3b27e5414ee7ff329824454acc
Stanley Wintergreen <kepocnhh@gmail.com>
2023-04-05 23:05:37 +0300

    Declarative Macros with macro_rules! for General Metaprogramming

--- src/book/ch19/mod.rs
+++ src/book/ch19/mod.rs
@@ -2,10 +2,12 @@ mod part01;
 mod part02;
 mod part03;
 mod part04;
+mod part05;
 
 pub fn run() {
     part01::run();
     part02::run();
     part03::run();
     part04::run();
+    part05::run();
 }
---
+++ src/book/ch19/part05.rs
@@ -0,0 +1,48 @@
+pub fn run() {
+    const CHAPTER: u8 = 19;
+    const PART: u8 = 5;
+    const TITLE: &str = "Macros";
+    println!("\n\t{:02}/{:02}\t\"{TITLE}\"", CHAPTER, PART);
+
+    _01();
+    todo!();
+}
+
+#[macro_export]
+macro_rules! num_args {
+    () => {
+        {
+            println!("no args");
+            0
+        }
+    };
+    ($it:expr) => {
+        {
+            println!("single: {}", $it);
+            1
+        }
+    };
+    ($($it:expr),*) => {
+        {
+            let mut number = 0usize;
+            $(
+                println!("arg[{number}]: {}", $it);
+                number += 1;
+            )*
+            number
+        }
+    };
+}
+
+fn _01() {
+    println!("\nDeclarative Macros with macro_rules! for General Metaprogramming");
+
+    let number = num_args!();
+    println!("number: {number}");
+    let number = num_args!(42);
+    println!("number: {number}");
+    let number = num_args!(1, 2, 3);
+    println!("number: {number}");
+    let number = num_args!(1, "foo", false);
+    println!("number: {number}");
+}
